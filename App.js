bdfd9b85dc6fd0d0ca2559a142e6202d689b52ff
nahbaratella <natybaratella05@gmail.com>
2024-03-07 10:48:20 -0300

    Rota

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Title from'./src/components/';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Nataly</Text>
+      <Text>Desenvolvimento de Dispositivos Móveis 07/mar2024</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
