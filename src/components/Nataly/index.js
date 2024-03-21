d590061a2f5b5c10a05d203ec6963d32045bf9c4
nahbaratella <natybaratella05@gmail.com>
2024-03-21 09:45:31 -0300

    Primeira prova DDM lado B

---
+++ src/components/Nataly/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Nataly() {
+    return(
+        <View>
+            <Text>Nataly Baratella</Text>
+            <Text>Pimeira prova DDM lado B</Text>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
